10cc6496da488fba4ea2d4d715ec33089855fe58
kjwjwjw <tbs03112@naver.com>
2024-07-11 21:09:42 +0900

    주문,상품 레포지토리, 서비스

--- src/main/java/jpabook/jpashop/domain/item/Movie.java
+++ src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -3,7 +3,6 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jpabook.jpashop.domain.Item;
 import lombok.Getter;
 import lombok.Setter;
 
