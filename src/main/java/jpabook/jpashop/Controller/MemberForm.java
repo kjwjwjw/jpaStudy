11878c1cbf9e2a5f2236b36da2633566f1a7c167
kjwjwjw <tbs03112@naver.com>
2024-07-20 00:30:21 +0900

    웹 계층 회원목록

---
+++ src/main/java/jpabook/jpashop/Controller/MemberForm.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.Controller;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class MemberForm {
+
+    @NotBlank(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
