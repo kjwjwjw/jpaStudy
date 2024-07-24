2ad4fce67f61ef9387d3da01018283ff460f3ddf
kjwjwjw <tbs03112@naver.com>
2024-07-24 21:17:08 +0900

    주문 취소, jpql 오류해결

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,11 +1,7 @@
 package jpabook.jpashop.domain;
-
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Cascade;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,51 +9,39 @@ import java.util.List;
 
 @Entity
 @Table(name = "orders")
-@Getter
-@Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter @Setter
 public class Order {
-
     @Id
     @GeneratedValue
     @Column(name = "order_id")
     private Long id;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
-    private Member member;
-
-    @OneToMany(mappedBy = "order")
+    private Member member; //주문 회원
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
-
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
-    @Cascade(org.hibernate.annotations.CascadeType.ALL)
-    private Delivery delivery;
-
-    private LocalDateTime orderDate; // 주문시간
-
+    private Delivery delivery; //배송정보
+    private LocalDateTime orderDate; //주문시간
     @Enumerated(EnumType.STRING)
-    private OrderStatus status; // 주문상태 [ORDER , CANCEL]
-
-    //== 연관관계 메서드 //
+    private OrderStatus status; //주문상태 [ORDER, CANCEL]
+    //==연관관계 메서드==//
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
     }
-
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
-
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
-
-    //==생성 메서드 ==//
-    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery,
+                                    OrderItem... orderItems) {
         Order order = new Order();
         order.setMember(member);
         order.setDelivery(delivery);
@@ -68,33 +52,24 @@ public class Order {
         order.setOrderDate(LocalDateTime.now());
         return order;
     }
-
-    //== 비즈니스 로직 ==//
-    /**
-     * 주문 취소
-     */
+    //==비즈니스 로직==//
+    /** 주문 취소 */
     public void cancel() {
         if (delivery.getStatus() == DeliveryStatus.COMP) {
             throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
         }
-
         this.setStatus(OrderStatus.CANCEL);
         for (OrderItem orderItem : orderItems) {
             orderItem.cancel();
         }
-
     }
-
-    //==조회 로직 ==//
-
-        /**
-         * 전체 주문 가격 조회
-         */
-        public int getTotalPrice() {
-            int totalPrice = 0;
-            for (OrderItem orderItem : orderItems) {
-                totalPrice += orderItem.getTotalPrice();
-            }
-            return totalPrice;
+    //==조회 로직==//
+    /** 전체 주문 가격 조회 */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
